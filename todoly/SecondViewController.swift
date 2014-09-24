7824b4b79474fd4534d3fcd8280d7a0d81dde7ef
Alex Ninneman <alex@ninneman.org>
2014-09-24 18:50:08 -0500

    Initial Commit

---
+++ todoly/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  todoly
+//
+//  Created by Alex Ninneman on 9/24/14.
+//  Copyright (c) 2014 Alex Ninneman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
